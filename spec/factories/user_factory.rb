a0a04cdb9bae82aa2789cfcdb3fd435e77076776
Sid Ngeth <sngeth@gmail.com>
2014-08-06 00:04:18 -0400

    Added admin flag to user.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,5 +4,8 @@ FactoryGirl.define do
     email "sample@example.com"
     password "test"
     password_confirmation "test"
+    factory :admin_user do
+      admin true
+    end
   end
 end
--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -5,7 +5,8 @@ module AuthenticationHelpers
     fill_in "Password", with: user.password
     click_button 'Sign in'
     expect(page).to have_content("Signed in successfully.")
-end end
+  end 
+end
 
 RSpec.configure do |c|
   c.include AuthenticationHelpers, type: :feature
